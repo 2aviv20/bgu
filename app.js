070719bc23cbe66ed8ba243a14c253ed4f046a6d
aviv <2aviv20@gmail.com>
2019-12-25 15:30:16 -0500

    port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const scraper = require("./scraper");
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.port | 3000;
+const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
